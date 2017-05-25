cf4cd1d1c578c4d169fe6f67efecbc6f68dbe720
Angel Ng <ang7@stuy.edu>
2017-05-25 10:40:13 -0400

    added driver

---
+++ Kart.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.util.PriorityQueue;
+
+public class Kart{
+
+    private PriorityQueue customers;
+    private ArrayStack burgers;
+    private int timeForLevel;
+    private int gameStart;
+    
+    //starts the level 
+    public void startLevel(){
+	customers = new PriorityQueue(); 
+    }
+
+    public void endScreen(){
+    }
+
+    public Customer nextCustomer(){
+    }
+
+    public void tossOrders(){
+    }
+
+    public int compareOrders(Burger other){
+    }
+
+    public static void main (String[] args){
+    }
+}
