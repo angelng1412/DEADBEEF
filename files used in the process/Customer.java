1c4d5b863dba515c844f12dcfa3f42c0ce47e277
Tina <tchen9@stuy.edu>
2017-06-07 01:48:32 -0400

    new folder

---
+++ files used in the process/Customer.java
@@ -0,0 +1,41 @@
+public class Customer implements Comparable{
+    private Hamburger _order; 
+    private int _priority;
+
+    //constructor
+    public Customer(int priority){
+	_order = new Hamburger();
+	_order.randomOrder(); 
+	_priority = priority; 
+    }
+
+    //accessor for _order
+    public Hamburger getOrder(){
+	return _order; 
+    }
+
+    //accessor for _priority
+    public int getPriority(){
+	return _priority; 
+    }
+
+    //compares priorities
+    public int compareTo(Object other){ 
+	if (this.getPriority() > ((Customer)other).getPriority()){
+	    return 1;
+	}
+	else if (this.getPriority() < ((Customer)other).getPriority()){
+	    return -1; 
+	}
+	else{
+	    return 0; 
+	}
+    }
+
+    public String toString(){
+	String retstr = "";
+	retstr += "ORDER: " + _order.toString() + "\n";
+	retstr += "PRIORITY LVL: " + _priority;
+	return retstr;
+    }
+}
---
+++ files used in the process/Hamburger.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+public class Hamburger{
+
+    private final String[] _ingredients = {"lettuce", "tomato", "pickle", "ketchup", "cucumber", "mustard", "cheese", "onion", "mushroom", "patty", "pepper"};
+    private Stack<String> _order;
+    private int _size; 
+
+    //constructor
+    public Hamburger(){
+	_order = new Stack();
+	_order.push("bottom bun"); 
+    }
+
+    //accessor for _order
+    public Stack<String> getOrder(){
+	return _order; 
+    }
+
+    public int size(){
+	return _size; 
+    }
+
+    //generates a random order
+    public void randomOrder(){
+	for (int x = 0; x < (int)(Math.random() * 3) + 2; x++){
+	    _order.push(_ingredients[(int)(Math.random() * 11)]);
+	    _size++;
+	}
+	_order.push("top bun"); 
+    }
+
+    //add to stack 
+    public void add(String ingredient){ 
+	_order.push(ingredient);
+    }
+
+    //compares burgers
+    public boolean isEqual(Hamburger other){
+	if (getOrder().size() != other.getOrder().size()){
+	    return false; 
+	}
+	else{
+	    for (int i = 0; i < getOrder().size(); i++){
+		if (getOrder().get(i) != other.getOrder().get(i)){
+		    return false;
+		}
+	    }
+	    return true; 
+	}
+    }
+
+    public String toString(){
+	String retstr = "";
+	for (int x = 0; x < _order.size(); x++){
+	    retstr += _order.get(x) + " ";
+	}
+	return retstr;
+    }
+    
+    public static void main(String[] args){
+	Hamburger bob = new Hamburger();
+	bob.randomOrder();
+        System.out.println(bob);
+    }
+}
